8d15cbe13fe36e5d5d476a44fb3ce865dd81d2ec
Sofiia13 <sofiia.yevush@gmail.com>
2024-04-25 23:42:08 +0300

    Sorting files

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+
+function init() { 
+    import('https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js');
+    import('./categories-carousel.js');
+    import('./testimonials-unregistered.js');
+}
+
+const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener('htmx:afterOnLoad', () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) init();
+});
