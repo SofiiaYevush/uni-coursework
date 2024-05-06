9fa805c27056999d61d3c3a920b925ac98cf1500
Sofiia13 <sofiia.yevush@gmail.com>
2024-05-06 16:45:42 +0300

    Add 'Sign up' page for tasker with styles

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function init() {
     import('https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js');
     import('./categories-carousel.js');
     import('./testimonials-unregistered.js');
+    import('./sign-up-tasker.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
