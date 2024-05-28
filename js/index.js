3c70f437ee86ca3528d284b910d850e14fb09288
Sofiia13 <sofiia.yevush@gmail.com>
2024-05-29 00:14:25 +0300

    Update button names

--- js/index.js
+++ js/index.js
@@ -7,11 +7,12 @@ function init() {
     import('./testimonials-unregistered.js');
     import('./sign-up-tasker.js');
 }
-
+/*
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
 let loadedPartialsCount = 0;
 
 document.body.addEventListener('htmx:afterOnLoad', () => {
     loadedPartialsCount++;
     if (loadedPartialsCount === totalPartials) init();
-});
+});*/
+
